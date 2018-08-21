cf679d48b9557c600f3bebb92018708eac0e8310
SebastianHC <SebastianHC@users.noreply.github.com>
2018-08-21 12:56:07 -0500

    no

--- dbconnect.php
+++
@@ -1,6 +0,0 @@
-<?php
-    //Conexion servidor
-    $connection = @mysql_connect("localhost","root","") or die("No se encontró el servidor");
-    //Conexion base de datos
-    mysql_select_db("bd_recuperacion",$connection)or die("No se encontró la base de datos");
-?>
